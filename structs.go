0ec1d3e31fff09b2a7cb5d9f55b7190d51bcb4a5
Domenic <dbarbuzzi@gmail.com>
2021-01-15 11:10:35 -0500

    Add new structs and helper to get auth token/sig from GQL API

--- structs.go
+++ structs.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"log"
 	"net/url"
 	"path/filepath"
@@ -56,4 +57,41 @@ type Chunk struct {
 	Length float64
 	URL    *url.URL
 	Path   string
+}
+
+// AuthGQLPayload represents the payload sent to the GQL endpoint to get the
+// auth token and signature
+type AuthGQLPayload struct {
+	OperationName string `json:"operationName"`
+	Query         string `json:"query"`
+	Variables     struct {
+		IsLive     bool   `json:"isLive"`
+		IsVod      bool   `json:"isVod"`
+		Login      string `json:"login"`
+		PlayerType string `json:"playerType"`
+		VodID      string `json:"vodID"`
+	} `json:"variables"`
+}
+
+func generateAuthPayload(vodID string) ([]byte, error) {
+	ap := AuthGQLPayload{
+		OperationName: "PlaybackAccessToken_Template",
+		Query:         "query PlaybackAccessToken_Template($login: String!, $isLive: Boolean!, $vodID: ID!, $isVod: Boolean!, $playerType: String!) {  streamPlaybackAccessToken(channelName: $login, params: {platform: \"web\", playerBackend: \"mediaplayer\", playerType: $playerType}) @include(if: $isLive) {    value    signature    __typename  }  videoPlaybackAccessToken(id: $vodID, params: {platform: \"web\", playerBackend: \"mediaplayer\", playerType: $playerType}) @include(if: $isVod) {    value    signature    __typename  }}",
+	}
+	ap.Variables.IsLive = false
+	ap.Variables.IsVod = true
+	ap.Variables.PlayerType = "site"
+	ap.Variables.VodID = vodID
+	return json.Marshal(ap)
+}
+
+// AuthGQLPayload represents the response from to the GQL endpoint containing
+// the auth token and signature
+type AuthGQLResponse struct {
+	Data struct {
+		VideoPlaybackAccessToken struct {
+			Value     string `json:"value"`
+			Signature string `json:"signature"`
+		} `json:"videoPlaybackAccessToken"`
+	} `json:"data"`
 }
